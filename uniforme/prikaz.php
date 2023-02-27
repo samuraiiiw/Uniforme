0dea5999705523c760ece13c98a0f5daddccd470
samuraiiiw <107076676+samuraiiiw@users.noreply.github.com>
2023-02-27 17:50:26 +0100

    test testic

--- uniforme/prikaz.php
+++ uniforme/prikaz.php
@@ -2,13 +2,13 @@
 
     require_once "functions.php";
 
-    $title = "prikazzzzzzzzzzzzz";
+    $title = "prikaz";
     $table= 'artikal';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
